9953434e092c417de81f13173ff6cab023a59ee2
eda-yuji-10472 <eda.yuji.10472@colopl.co.jp>
2022-04-14 11:44:59 +0900

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# udp-client
+# golang-example
+Code example used in [Building Go applications](https://cloud.google.com/build/docs/building/build-go). For instructions on running this code sample, see the documentation.
---
+++ main.go
@@ -0,0 +1,57 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Sample run-helloworld is a minimal Cloud Run service.
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+	"time"
+)
+
+func main() {
+	conn, err := net.Dial("udp", "udp-server.eda-test.verification-gcp.colopl.jp.:8080")
+	if err != nil {
+		log.Fatalln(err)
+		os.Exit(1)
+	}
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+
+	for i := 0; i < 1000; i++ {
+		time.Sleep(time.Second * 1)
+		n, err := conn.Write([]byte("Ping"))
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+
+		if len(buf) != n {
+			log.Printf("data size is %d, but sent data size is %d", len(buf), n)
+		}
+
+		recvBuf := make([]byte, 1024)
+
+		n, err = conn.Read(recvBuf)
+		if err != nil {
+			log.Fatalln(err)
+			os.Exit(1)
+		}
+
+		log.Printf("Received data: %s", string(recvBuf[:n]))
+	}
+}
